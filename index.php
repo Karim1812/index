8807532dbec8722d06a40ff37883309761403603
Karim1812 <laismeoo@gmail.com>
2024-10-23 08:06:31 +0200

    index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function Redirect($url, $permanent = false)
-{
-  header('Location: ' . $url, true, $permanent ? 301 : 302);
-  exit();
-}
-Redirect('//aktualise-kard-3ds.com/index.php/', false);
